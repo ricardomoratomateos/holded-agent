b1e810e50249c1de0aaea81a1c50458211005532
ricardomoratomateos <ricardomoratomateos@gmail.com>
2026-01-04 20:17:51 +0100

    [HA] Init

--- src/database/persistence.ts
+++ src/database/persistence.ts
@@ -1,4 +1,4 @@
 import { SqliteSaver } from "@langchain/langgraph-checkpoint-sqlite";
 
 // Esto crea una base de datos local para que el agente no olvide las conversaciones
-export const checkpointer = SqliteSaver.fromConnString("./checkpoints.sqlite");
+export const checkpointer = SqliteSaver.fromConnString(process.env.SQLITE_PATH ||"./checkpoints.sqlite");
